1af0d190b28bb35d9e0cc3439620b02c90280b9d
bobadillajuan <juampi348@gmail.com>
2022-04-06 21:38:28 -0300

    agrego tp2_1_1

---
+++ tp2_1_1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdio.h>
+#include<time.h>
+
+#define N 20
+
+int main(){
+
+int i;
+double vt[N];
+
+for (i = 0; i < N; i++)
+{
+    
+    vt[i]=1 + rand()%100;
+    printf("%.1f  ", vt[i]);
+}
+
+
+return 0;
+}
